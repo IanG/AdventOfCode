5cfe3eae9dab2c662fab90b0252fd4998cd97077
Gratton, Ian <Ian.Gratton@experian.com>
2021-12-07 16:36:43 +0000

    Day 7 - Part 2 (Brute Force)

--- 2021/Day7/Program.cs
+++ 2021/Day7/Program.cs
@@ -11,21 +11,30 @@ namespace Day7
             List<int> inputs = System.IO.File.ReadAllText(args[0])
                 .Split(',').Select(value => int.Parse(value)).ToList();
 
-            int lowestCost = GetLowestFuelCostToAlignCrabs(inputs);
-
+            int lowestCost = GetLowestFuelCostToAlignCrabs(inputs, false);
             Console.WriteLine($"Answer 1: {lowestCost}");
+
+            lowestCost = GetLowestFuelCostToAlignCrabs(inputs, true);
+            Console.WriteLine($"Answer 2: {lowestCost}");
         }
 
-        private static int GetLowestFuelCostToAlignCrabs(List<int> crabs)
+        private static int GetLowestFuelCostToAlignCrabs(List<int> crabs, bool slidingFuelCost)
         {
             List<int> costs = new List<int>();
 
-            foreach (int position in crabs.Distinct())
+            for (int position = crabs.Min(); position < crabs.Max(); position++)
             {
                 int cost = 0;
                 for (int crab = 0; crab < crabs.Count; crab++)
                 {
-                    cost+= Math.Abs(crabs[crab] - position);
+                    if (slidingFuelCost)
+                    {
+                        cost += Enumerable.Range(1, Math.Abs(crabs[crab] - position - 1)).Sum();
+                    }
+                    else 
+                    {
+                        cost += Math.Abs(crabs[crab] - position);
+                    }
                 }
                 costs.Add(cost);
             }
