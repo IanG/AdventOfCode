ca9df649489803e2cdb3fb1b023b18835151b14a
Gratton, Ian <Ian.Gratton@experian.com>
2021-12-07 12:35:13 +0000

    Day 7 - Part 1

---
+++ 2021/Day7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputs = System.IO.File.ReadAllText(args[0])
+                .Split(',').Select(value => int.Parse(value)).ToList();
+
+            int lowestCost = GetLowestFuelCostToAlignCrabs(inputs);
+
+            Console.WriteLine($"Answer 1: {lowestCost}");
+        }
+
+        private static int GetLowestFuelCostToAlignCrabs(List<int> crabs)
+        {
+            List<int> costs = new List<int>();
+
+            foreach (int position in crabs.Distinct())
+            {
+                int cost = 0;
+                for (int crab = 0; crab < crabs.Count; crab++)
+                {
+                    cost+= Math.Abs(crabs[crab] - position);
+                }
+                costs.Add(cost);
+            }
+
+            return costs.Min();
+        }
+    }
+}
