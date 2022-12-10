a5c8f66196ae89cae638ed66df40fcdd73ad3d84
Ian Gratton <iandgratton@googlemail.com>
2022-12-10 18:18:43 +0000

    2022 - Day 1

---
+++ 2022/Day1/Program.cs
@@ -0,0 +1,22 @@
+﻿IEnumerable<string> lines = System.IO.File.ReadLines(args[0]);
+
+int maxCalories = 0;
+int currentCalories = 0;
+
+foreach (string line in lines)
+{
+    if (String.IsNullOrEmpty(line))
+    {
+        if (currentCalories > maxCalories)
+        {
+            maxCalories = currentCalories;
+        }
+        currentCalories = 0;
+    }
+    else
+    {
+        currentCalories += Convert.ToInt32(line);
+    }
+}
+
+Console.WriteLine($"Max Calories {maxCalories}.");
