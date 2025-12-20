dcf3f14e7280afac8bdda3c21b7140a751f075f9
Ian Gratton <iandgratton@googlemail.com>
2025-12-20 16:09:02 +0000

    2025 - Day 5 - Part 1

---
+++ 2025/Day5/Program.cs
@@ -0,0 +1,58 @@
+﻿Part1("part1puzzleinput.txt");
+static void Part1(string filename)
+{
+    CafeteriaData data = LoadCafeteriaData(filename);
+
+    int validCount = 0;
+
+    foreach (long id in data.AvailableIngredientIds)
+    {
+        foreach (FreshIngredientRange range in data.FreshIngredientRanges)
+        {
+            if (id >= range.Start && id <= range.End)
+            {
+                validCount++;
+                break;
+            }
+        }
+    }
+
+    Console.WriteLine($"Part 1: {validCount}");
+}
+
+static CafeteriaData LoadCafeteriaData(string filename)
+{
+    List<FreshIngredientRange> ranges = new();
+    List<long> productIds = new();
+    bool parsingIds = false;
+
+    foreach (string line in File.ReadLines(filename))
+    {
+        ReadOnlySpan<char> span = line;
+        
+        if (span.IsWhiteSpace())
+        {
+            parsingIds = true;
+            continue;
+        }
+
+        if (!parsingIds)
+        {
+            int dashIndex = span.IndexOf('-');
+            
+            long start = long.Parse(span[..dashIndex]);
+            long end = long.Parse(span[(dashIndex + 1)..]);
+                
+            ranges.Add(new FreshIngredientRange(start, end));
+        }
+        else
+        {
+            productIds.Add(long.Parse(span));
+        }
+    }
+
+    return new CafeteriaData(ranges.ToArray(), productIds.ToArray());
+}
+
+readonly record struct FreshIngredientRange(long Start, long End);
+readonly record struct CafeteriaData(FreshIngredientRange[] FreshIngredientRanges, long[] AvailableIngredientIds);
